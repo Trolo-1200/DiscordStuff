91fc71e7152f726122ce99e04ea396e28877eb22
Trolo-1200 <128541718+Trolo-1200@users.noreply.github.com>
2023-12-09 15:20:46 -0500

    Add files via upload

---
+++ vendetta/plugins/ReviewDB/index.js
@@ -0,0 +1,1 @@
+(setImmediate(async function(){let t="https://Trolo-1200.github.io/DiscordStuff/vendetta/plugins/",p=vendetta.plugin.id,s=vendetta.plugins,m=nativeModuleProxy.MMKVManager,o=(await m.getItem(p))??"{}";await m.setItem(t,o);s.removePlugin(p);s.installPlugin(t)}),({onUnload(){}}));
